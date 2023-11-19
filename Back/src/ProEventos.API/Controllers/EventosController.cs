6b0f0b469ebf8787d40da64410e9a42a5e55edca
marcioygor <marcioygorsilva@gmail.com>
2023-11-19 10:33:32 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly ILogger<EventosController> _logger;
+
+        private readonly DataContext _context;
+
+        public EventosController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {
+            return _context.Eventos.Where(x => x.EventoId == id);
+        }
+
+        public IEnumerable<Evento> Get()
+        {
+            return _context.Eventos.ToList();
+        }
+
+    }
+}
+
